ca0e1b583a1421b44f872f63b11eed2f40d1ba77
Arshdeep <arshdeep.singh86@gmail.com>
2016-04-15 12:48:30 +0530

    Added files via upload

---
+++ ElevatorAlgorithm.java
@@ -0,0 +1,9 @@
+/** ElevatorAlgorithm interface 
+ * YOU SHOULD NOT NEED TO MODIFY THIS CODE
+ */
+
+public interface ElevatorAlgorithm {
+  // methods that all elevator algorithm classes must implement
+  public void setController(ElevatorController controller);
+  public void move();
+}
